5ee8912d93c4112d0f11f589a98f404c7030f538
Lucas Shriver <34632413+lshriver2@users.noreply.github.com>
2020-03-22 11:37:03 -0500

    Add files via upload

--- gui main pi rocket.py
+++ gui main pi rocket.py
@@ -37,6 +37,24 @@ for i in range(1):
 # changes.  Remember altitude estimation from barometric pressure is not exact!
 # Set this to a value in pascals:
 
+pressure2 = 0
+pressure1 = 0
+altitude = 0
+temperature = 0
+
+
+# Open function to open the file "MyFile1.txt"  
+# (same directory) in append mode and 
+file1 = open("MyFile.txt","a")
+
+'''
+# store its reference in the variable file1  
+# and "MyFile2.txt" in D:\Text in file2 
+file1 = open("MyFile1.txt","w+") 
+file1.close()
+'''
+
+
 
 def mprls():
     global pressure2
@@ -45,7 +63,7 @@ def mprls():
     mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25)
     mprIntScale = int(mpr.pressure)
     mprInt = mprIntScale * 100
-    pressure2 = 'Pressure2: {0:0.3f} pascals'.format(mprInt)
+    pressure2 = 'Nose Cone Pressure: {0:0.3f} pascals'.format(mprInt)
 
 def mpl3115a2():
     global pressure1
@@ -75,59 +93,56 @@ def buttonC():
     global altitude
     global temperature
     
-    mprls()
-    mpl3115a2()
+    getData()
     
     print(pressure2 +'    '+ pressure1 +'    '+  altitude +'    '+  temperature)
-    '''
-    print(pressure1)
-    print(altitude)
-    print(temperature)
-    print('---------')
-'''
-    
 
-    
 def runRun():
     global pressure2
     global pressure1
     global altitude
     global temperature
-    loop = int(input("enter number for loop "))
+#     loop = int(input("enter number for loop "))
     
-    for i in range(loop):
-        getData()
-        print(pressure2 +'    '+ pressure1 +'    '+  altitude +'    '+  temperature)
-        sleep(0.01)
+    telemetryPrint = pressure2 +'    '+ pressure1 +'    '+  altitude +'    '+ temperature +'    '+ dt.datetime.now().strftime('%m-%d-%Y %H:%M:%S')
 
-#remove forget
-def forget2():
-    w1.grid_remove()
-    w2.grid_remove()
-    w3.grid_remove()
-    w4.grid_remove()
+    getData()
+    print(telemetryPrint)
+    file1.write(telemetryPrint +"\n")
+    
 
 getData()
 
-#l = Label(root, text= pressure2)
-#l.pack()
-'''
-w1 = Label(root, text = pressure2, bg="red", fg="white")
-w2 = Label(root, text = pressure1, bg="green", fg="black")
-w3 = Label(root, text = altitude, bg="blue", fg="white")
-w4 = Label(root, text = temperature, bg="brown", fg="white")
+width = 40
+height = 2
+font = 1
+font = ("Courier", 24)
+
+w1 = Label(root, text = pressure2, bg="tan", fg="black", width = width, height = height, font = font)
+w2 = Label(root, text = pressure1, bg="white", fg="black", width = width, height = height, font = font)
+w3 = Label(root, text = altitude, bg="tan", fg="black", width = width, height = height, font = font)
+w4 = Label(root, text = temperature, bg="white", fg="black", width = width, height = height, font = font)
 
 w1.pack()
 w2.pack()
 w3.pack()
 w4.pack()
 
-global w1
-global w2
-global w3
-global w4
-'''
+def config():
+    global w1
+    global w2
+    global w3
+    global w4
 
+    w1.config(text = pressure2)
+    w2.config(text = pressure1)
+    w3.config(text = altitude)
+    w4.config(text = temperature)
+    
+    w1.pack()
+    w2.pack()
+    w3.pack()
+    w4.pack()
 
 def remember():
     global w1
@@ -136,15 +151,10 @@ def remember():
     global w4
 
     getData()
-    w1 = Label(root, text = pressure2, bg="red", fg="white")
-    w2 = Label(root, text = pressure1, bg="green", fg="black")
-    w3 = Label(root, text = altitude, bg="blue", fg="white")
-    w4 = Label(root, text = temperature, bg="brown", fg="white")
-#     l.pack()
-    w1.pack()
-    w2.pack()
-    w3.pack()
-    w4.pack()
+    config()
+    
+    runRun()
+    root.after(100, remember)
 
 def forget():
     global w1
@@ -152,8 +162,6 @@ def forget():
     global w3
     global w4
     
-#    l.pack_forget()
-    #w1.after(6000, w1.pack_forget())
     w1.pack_forget()
     w2.pack_forget()
     w3.pack_forget()
@@ -162,35 +170,33 @@ def forget():
 
 def quick():
     remember()
-    forget()
+    #forget()
+    remember()
     
-b = Button(root, text="Forget about it", command=forget)
-b.pack()
-b2 = Button(root, text="Wait, I still need you", command=remember)
-b2.pack()
-b3 = Button(root, text="run quick", command= quick)
-b3.pack()
-button1 = Button(root, text = 'B', command = buttonC)
-button1.pack()
-    
-'''
-w1 = Label(root, text = ' ', bg="red", fg="white").grid(row=0,column=0)
-w2 = Label(root, text = ' ', bg="green", fg="black").grid(row=1,column=0)
-w3 = Label(root, text = ' ', bg="blue", fg="white").grid(row=2,column=0)
-w4 = Label(root, text = ' ', bg="brown", fg="white").grid(row=3,column=0)
-button1 = Button(root, text = 'B', command = buttonC).grid(row=4,column=0)
-button2 = Button(root, text = 'B', command = forget).grid(row=5,column=0)
-'''
-       
-'''
-w1 = Label(root, text = pressure2, bg="red", fg="white").grid(row=0,column=0)
-w2 = Label(root, text = pressure1, bg="green", fg="black").grid(row=1,column=0)
-w3 = Label(root, text = altitude, bg="blue", fg="white").grid(row=2,column=0)
-w4 = Label(root, text = temperature, bg="brown", fg="white").grid(row=3,column=0)
-button1 = Button(root, text = 'B', command = buttonC).grid(row=4,column=0)
-button2 = Button(root, text = 'forget', command = forget).grid(row=5,column=0)
-'''
-mainloop()
 
-# runRun()
+frame1 = Frame(root)
+frame1.pack()
+'''
+bforget = Button(frame1, text="Forget", command=forget)
+bforget.pack()
+bremember2 = Button(frame1, text="Remember", command=remember)
+bremember2.pack(side = RIGHT)
+brunQuick = Button(frame1, text="Run Quick", command= quick)
+brunQuick.pack(side = RIGHT)
+buttonC = Button(frame1, text = 'Button C', command = buttonC)
+buttonC.pack(side = RIGHT)
+'''
+buttonQ = Button(frame1, text = 'Quit', command = root.destroy, width = width, font = ("Courier", 16))
+buttonQ.pack()
+'''
+re = 0
+if re == 0:
+    re = re+1'''
+remember()
+  
+#root.after(100, remember)
+mainloop()
+file1.close()
+print("File Closed")
+
 
